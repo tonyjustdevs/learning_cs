38e10e3f152771f9a9d2971b21d15dd420bec397
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-26 01:28:21 +0700

    add VarKeyword_1_2.sln 260925

---
+++ microsoft/sec1_consoleapps/VarKeyword_1_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var name = "ange"; // 1. var keyword string
+var age = 69; // 2. var keyword double
+
+Console.WriteLine($"name: {name}");
+Console.WriteLine($"age: {age}");
