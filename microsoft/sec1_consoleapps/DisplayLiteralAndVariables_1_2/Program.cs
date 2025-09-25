03bb2982af577dd1e2dcc8f42b506e92d9eab89c
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-26 02:14:19 +0700

    add DisplayLiteralAndVariables_1_2.sln 260925

---
+++ microsoft/sec1_consoleapps/DisplayLiteralAndVariables_1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// 1.  store 3 variables
+// 2a. use Console.Write() to display
+// 2b. "Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius."
+
+var name    = "Bob";
+var msgs    = 3;
+var celsius = 34.4;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(msgs);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(celsius);
+Console.Write(" celsius");
