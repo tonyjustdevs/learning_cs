2ce68b9fe8f4f3a11c008251d6a12900b0de8f0b
tony justdevs <tonyjustdevs@gmail.com>
2025-09-26 03:19:26 +0700

    update invoice VerbatimStringLiteral_1_3 260925

--- microsoft/sec1_consoleapps/VerbatimStringLiteral_1_3/Program.cs
+++ microsoft/sec1_consoleapps/VerbatimStringLiteral_1_3/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(@"Hello, 
-
-mate
-
-
-World!");
+Console.WriteLine("Generating invoices for customer \"Ange Corp\" ... \n");
+Console.WriteLine("Invoice: 1069\t\tComplete!");
+Console.WriteLine("Invoice: 1420\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\tonys_invoices");
+Console.WriteLine();
