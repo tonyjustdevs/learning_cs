05ba8360821cebe93386758947e001faa9caf79f
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-26 03:08:05 +0700

    add OutputEscapeSeqs_1_3.sln 260925

---
+++ microsoft/sec1_consoleapps/OutputEscapeSeqs_1_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+string customer = "Contoso Corp";
+int invoice_no_1 = 1021;
+int invoice_no_2 = 1022;
+
+Console.WriteLine($"Generating invoices for customer {customer}\n");
+
+Console.WriteLine($"Invoice: {invoice_no_1}\t\tComplete!");
+Console.WriteLine($"Invoice: {invoice_no_2}\t\tComplete!\n");
+
+Console.WriteLine($"Output Directory:");
+
+//display below
+//Generating invoices for customer "Contoso Corp" ...
+
+//Invoice: 1021           Complete!
+//Invoice: 1022           Complete!
+
+//Output Directory:
