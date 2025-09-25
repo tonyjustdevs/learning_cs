d82a07ceaf864b32896c054471ac8fff933719cd
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-26 01:27:49 +0700

    add DeclaringVariables_1_2.sln 260925

---
+++ microsoft/sec1_consoleapps/DeclaringVariables_1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. declare string
+char chr;
+chr = 'a';
+
+string some_str;
+some_str = "mate";
+
+Console.WriteLine($"declared chr: {chr}");
+Console.WriteLine($"declared str: {some_str}");
+
+chr = 'b';
+some_str= "ange";
+
+Console.WriteLine($"reassn chr: {chr}");
+Console.WriteLine($"reassn str: {some_str}");
+
+// 2. reassign string
+
