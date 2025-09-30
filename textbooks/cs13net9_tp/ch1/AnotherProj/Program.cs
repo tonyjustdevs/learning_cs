316f7da8a880877e8e86c37b6a237b3c49c1f115
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-30 07:37:02 +0700

    add ch1 cs23net9_tp

---
+++ textbooks/cs13net9_tp/ch1/AnotherProj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AnotherProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
