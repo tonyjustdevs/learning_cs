320d468e72b2c0d4a5b6141af267d1ea6062408b
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-01 18:05:14 +0700

    add PartialProgram_3_18_1.sln 011025

---
+++ textbooks/essentialcs/ch18/PartialProgram3_18_1/Program.Functions.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+partial class Program
+{
+    static void gday() => WriteLine("gday \u1986\u1987");
+}
---
+++ textbooks/essentialcs/ch18/PartialProgram3_18_1/Program.cs
@@ -0,0 +1,3 @@
+﻿
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+gday();
