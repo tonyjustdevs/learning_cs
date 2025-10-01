e7bbd57992d10c343bee0830481aac42603ed8b4
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-01 10:24:32 +0700

    add chp2 cs13net9_tp 300925

---
+++ textbooks/cs13net9_tp/ch2/ShowAllTypes_2_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Get the assembly that is the entry point for this app.
+
+using System.Reflection;
+Assembly? myApp = Assembly.GetEntryAssembly();
+
+DateTime when = new(2000, 06, 12);
+
+if (myApp is null) return;                      // If the previous line returned nothing then end the app.
+
+foreach (AssemblyName name in myApp.GetReferencedAssemblies()) // Loop through the assemblies that my app references.
+{
+    Assembly a = Assembly.Load(name); // Load the assembly
+    int methodCount = 0;                        // Declare variable to count number of methods.
+    foreach (TypeInfo t in a.DefinedTypes)      // Loop through all types in assembly.
+    {
+        methodCount += t.GetMethods().Length;   // Add counts of all methods.
+    } 
+    WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.", 
+      arg0: a.DefinedTypes.Count(),             // Output the count of types and their methods.
+      arg1: methodCount,
+      arg2: name.Name);
+}
+
+
---
+++ textbooks/cs13net9_tp/ch2/ShowCompilerVersion_2_1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+//#error version
+//Console.WriteLine("Hello, World!");
+WriteLine($"Computer named {Env.MachineName} says \"No.\"");
