6a1e6c53ec45970948eee45a4213fdfab32a7008
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-01 17:59:02 +0700

    add PartialProgram2_18_1.sln 100125

---
+++ textbooks/essentialcs/ch18/PartialProgram2_18_1/LetsGo.cs
@@ -0,0 +1,8 @@
+﻿
+using static System.Console;
+
+partial class Program
+{
+    static void gday() => WriteLine("Hello, World! \u1986\u1987");
+
+}
---
+++ textbooks/essentialcs/ch18/PartialProgram2_18_1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.OutputEncoding =  System.Text.Encoding.UTF8;
+gday();
+//void gday() => WriteLine("Hello, World! \u1986\u1987");
