5c86cae8b87716090b3a5b5f37a8ce3a22e08b6b
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-01 17:11:58 +0700

    add GetTypeNamespace_18_1.sln 011025

---
+++ textbooks/essentialcs/ch18/GetTypeNamespace_18_1/Program.Functions.cs
@@ -0,0 +1,11 @@
+﻿partial class Program
+{
+    static void ShowMyNameSpace()
+    {
+        throw new Exception();
+        string prog_namespace = typeof(Program).Namespace ?? "<null>";
+
+        Console.WriteLine("namespace: {0}",
+            prog_namespace);
+    }
+}
