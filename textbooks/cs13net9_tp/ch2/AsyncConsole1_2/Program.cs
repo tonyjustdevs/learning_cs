40c4d631ad7463f073afc163e0cdd9e4d1c0c4e2
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-02 14:13:06 +0700

    add AsyncConsole1_2 021025

---
+++ textbooks/cs13net9_tp/ch2/AsyncConsole1_2/Program.cs
@@ -0,0 +1,9 @@
+﻿
+HttpClient client = new();
+
+HttpResponseMessage response = 
+    await client.GetAsync("https://www.apple.com");
+
+
+Console.WriteLine("http_response: {0:N0} bytes", response.Content.Headers.ContentLength);
+//http_response: 215,626 bytes
