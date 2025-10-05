0b7b94e7848141bfb3bcd40e8d1599ecdc77da68
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-05 15:07:14 +0700

    add Switcheroo_3_2.sln 051025

---
+++ textbooks/cs13net9_tp/ch3/Switcheroo_3_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int rando_int = Random.Shared.Next(0, 5);
+
+switch (rando_int)
+{
+    case 1:
+        WriteLine("winner 1, chicken dinner!");
+        break;
+    case 2:
+        WriteLine("2s company!");
+        goto case 1;
+    case 3:
+    case 4:
+        WriteLine("runner-upperers 3 & 4...");
+        break;
+    default:
+        WriteLine("loser 5 the jive :/");
+        break;
+}
