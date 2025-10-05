0ff8ae0cd316391f390830488254c11f81ba817d
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-05 08:53:51 +0700

    add model logic in comms NullCoalescing_3_1.sln 051025

---
+++ textbooks/cs13net9_tp/ch3/NullCoalescing_3_1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// 1.  create fictional function and assign to a str
+
+// 2.  apply null coalescing logic:
+// 2a. - MaxLength variable is length of authorName if not null,
+// 2b. - 30 if authorName is null.
+
+// 3. authorName variable will be "unknown" if authorName was null.
+
+
