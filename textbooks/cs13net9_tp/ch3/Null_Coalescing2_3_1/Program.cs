299880b60eb2948f9a42b03b0d07f758cc2e1d63
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-05 09:24:28 +0700

    add sgl script Null_Coalescing2_3_1.sln 0515025

---
+++ textbooks/cs13net9_tp/ch3/Null_Coalescing2_3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// 1.  get [authorName] str
+string? authorName = GetAuthorName(false);
+
+// 2.  null-coalesc logic
+// 2a. return [authorName.Length]   if not  [null]
+// 2b. return [30]                  if      [null]
+int authorNameLength = authorName?.Length ?? 69;
+
+// 3. authorName "unknown"          if      [null]
+authorName ??= "mystery man";
+
+// 4. print
+Console.WriteLine("{0}, {1}", authorName, authorNameLength);
+string? GetAuthorName(bool isAuth = true)
+{
+	if (isAuth)
+	{
+		return "old buddy";
+	}
+	return null;
+}
