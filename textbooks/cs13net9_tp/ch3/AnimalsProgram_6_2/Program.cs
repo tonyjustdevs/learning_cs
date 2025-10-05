4126f015b6c8607a2684c689862595a595095d68
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-05 15:16:17 +0700

    add sgl script 3 clses AnimalsProgram_6_2.sln 051025

---
+++ textbooks/cs13net9_tp/ch3/AnimalsProgram_6_2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Animal
+{
+    public string? Name;
+    public DateOnly Born;
+    public byte Legs;
+}
+
+class Dog : Animal
+{
+    public bool isDomesticated;
+}
+
+class Snake : Animal
+{
+    public bool isVenomous;
+}
