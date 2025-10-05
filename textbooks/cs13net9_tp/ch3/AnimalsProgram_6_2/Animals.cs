6c1ea3888e8751b62443c0690adda88c7126aa32
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-05 15:55:59 +0700

    add AnimalsProgram_6_2.sln 051025

---
+++ textbooks/cs13net9_tp/ch3/AnimalsProgram_6_2/Animals.cs
@@ -0,0 +1,16 @@
+﻿class Animal
+{
+    public string? Name;
+    public DateOnly Born;
+    public byte Legs;
+}
+
+class Dog : Animal
+{
+    public bool isDomesticated;
+}
+
+class Snake : Animal
+{
+    public bool isVenomous;
+}
--- textbooks/cs13net9_tp/ch3/AnimalsProgram_6_2/Program.cs
+++ textbooks/cs13net9_tp/ch3/AnimalsProgram_6_2/Program.cs
@@ -1,17 +1,22 @@
-﻿
-class Animal
-{
-    public string? Name;
-    public DateOnly Born;
-    public byte Legs;
-}
+﻿// 1. declare an array of nullable animals,
+// 2. show a message based on what type and attributes each animal has
 
-class Dog : Animal
+var animals = new Animal?[]
 {
-    public bool isDomesticated;
-}
+    new Dog{Name="old dawgy dawg", Legs=4, Born=new DateOnly(year: 2000, month: 12, day: 25), isDomesticated=true},
+    null,
+    new Dog{Name="lil pupz", Legs=8, Born=new DateOnly(year: 2020, month: 01, day: 26),isDomesticated=false},
+    new Snake{Name="imma snek", Legs=0, Born=new DateOnly(year: 1930, month: 02, day: 28),isVenomous=true},
+};
 
-class Snake : Animal
+foreach (Animal? curr_animal in animals)
 {
-    public bool isVenomous;
+    if (curr_animal is null) {
+        continue;
+    };
+    WriteLine("Name: {0}, a {1}, was born on {2} with {3} legs.", 
+        curr_animal?.Name,
+        curr_animal?.GetType(),
+        curr_animal?.Born,
+        curr_animal?.Legs);
 }
