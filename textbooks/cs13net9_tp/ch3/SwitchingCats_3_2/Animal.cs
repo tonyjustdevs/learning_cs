9d70feac65ab7c592d452eabd41f1b3d0dcd7b59
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-07 03:18:11 +0700

    add SwitchingCats_3_2.sln 071025

---
+++ textbooks/cs13net9_tp/ch3/SwitchingCats_3_2/Animal.cs
@@ -0,0 +1,21 @@
+﻿
+
+class Animal                // A) base animal cls
+{
+    public string? Name;
+    public byte Legs;
+    public DateOnly DOB;
+}
+
+class Cat : Animal          // B. create cat cls
+{
+    bool isDomesticated;
+}
+
+class Snake : Animal          // B. create SNAKE cls
+{
+    bool isVenomous;
+}
+
+
+
---
+++ textbooks/cs13net9_tp/ch3/SwitchingCats_3_2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//Cat cat1 = new Cat { Name = "Oreo", Legs = 4, DOB = new DateOnly(2020, 12, 01) };
+//Snake snek1 = new Snake { Name = "Hisser", Legs = 0, DOB = new DateOnly(1970, 12, 01) };
+
+//List<Animal> sneks_and_cats = [cat1, snek1];
+
+//var animals = new Animal?[]{
+List<Animal> sneks_and_cats = [
+    null,
+    new Cat {"Oreo", 4, new DateOnly(2018,01, 14) }];
+    //new Cat { Name = "Oreo", Legs = 4, DOB = new DateOnly(2020, 12, 01) }];
+    //new Cat { "Oreo", 4, new DateOnly(year: 2018, 9, 14) },
+    //new Cat {"Lilo", 4, new DateOnly(2020,10, 30) },
+    //new Snake {"Hissy",0, new DateOnly(2024,1,15) }
+//};
+
+foreach (Animal animal in sneks_and_cats)
+{
+    WriteLine("{0},{1},{2},{3}", animal.Name, animal.DOB.ToString(), animal.Legs, animal.GetType());
+}
+
+
+
+// loop list output assign [msg] var:
+// 1) cat + fourLeggedCat:  [Legs]  = 4
+// 2) cat + wildcat:        [isDom] = False
+// 3) cat                   
+// 4) default
+// 5) spider                [isVen] = True
+// 6) null                  
+
+// 7) output var
