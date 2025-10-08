c895ed29b1b62dab9878d706555590b8f7f307bf
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-08 16:33:43 +0700

    add ForEach_Vs_IEnumerator_3_3.sln 081025

---
+++ textbooks/cs13net9_tp/ch3/ForEach_Vs_IEnumerator_3_3/Program.cs
@@ -0,0 +1,28 @@
+﻿//// v1: foreach
+//string[] names = { "mate", "ange", "mourinho", "pep" };
+//foreach (var name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+
+// v2: IEnumerator
+//using System.Collections;
+using System.Collections;
+Console.WriteLine();
+string[] names = { "mate", "ange", "mourinho", "pep" };
+IEnumerator name_rator= names.GetEnumerator();
+while (name_rator.MoveNext())
+{
+    //Console.WriteLine($"name_rator.Current: {name_rator.Current}, type: {name_rator.Current.GetType()}");
+    Console.WriteLine($"name_rator.Current: {(int)name_rator.Current}");
+}
+
+// foreach: any type following rules:
+// The type has methido [GetEnumerator()] => returns an object.
+// Return object has
+//  - .property  [.Current]
+//  - .method(): [MoveNext()]
+//      -   MoveNext()
+//      -   RETURNS [true]:     chgs value of Current [if more items] or
+//      -   RETURNS [false]:    [if no more items].
