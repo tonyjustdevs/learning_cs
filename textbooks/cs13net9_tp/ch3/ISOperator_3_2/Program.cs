3bf6d383cc0ad67dd7c72846cbe8ee30965e46e0
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-08 13:02:05 +0700

    add ISOperator_3_2.sln 081025

---
+++ textbooks/cs13net9_tp/ch3/ISOperator_3_2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//object o = "69";
+object o = 69;
+int j = 420;
+
+if (o is int z)
+{   // if o is int then apply arithmetic: o+z
+    Console.WriteLine($"o is an integer (o[{o}]+j[{j}]): {z + j}");
+    //Console.WriteLine($"o is an integer (o[{o}]+j[{j}]): {z + j}");
+}
+else
+{
+    Console.WriteLine($"{o} is not an integer!");
+}
