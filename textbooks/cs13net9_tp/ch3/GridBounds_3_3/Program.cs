465f4c02aab1e402d52fa216f5952ba633bf9982
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-08 17:44:17 +0700

    add loop elements via lbound & ubound 081025

--- textbooks/cs13net9_tp/ch3/GridBounds_3_3/Program.cs
+++ textbooks/cs13net9_tp/ch3/GridBounds_3_3/Program.cs
@@ -12,9 +12,39 @@ string[,] names_grid = new string[,] {
     { "aaasda", "bbasdfb", "ccasdfc"},
 };
 
+//[0, 0]: aaaa
+//[0, 1]: aa
+//[0, 2]: aaa
+//[1, 0]: bbbb
+//[1, 1]: bb
+//[1, 2]: bbb
+
 
 Console.WriteLine($"names_grid.GetLowerBound(0): {names_grid.GetLowerBound(0)} (EXP: 0)");
 Console.WriteLine($"names_grid.GetUpperBound(0): {names_grid.GetUpperBound(0)} (EXP: 3)");
 
 Console.WriteLine($"names_grid.GetLowerBound(1): {names_grid.GetLowerBound(1)} (EXP: 0)");
 Console.WriteLine($"names_grid.GetUpperBound(1): {names_grid.GetUpperBound(1)} (EXP: 2)");
+
+
+// loop through grid with lbounds & ubounds
+
+for (int row = 0; row <= names_grid.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= names_grid.GetUpperBound(1); col++)
+    {
+        Console.WriteLine($"[{row},{col}]: {names_grid[row,col]}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
