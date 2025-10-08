52fc7f78d9a691b4f27758df62d4808429142f4a
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-08 13:11:22 +0700

    add BranchingWithSwitch_3_2.sln 081025

---
+++ textbooks/cs13net9_tp/ch3/BranchingWithSwitch_3_2/Program.cs
@@ -0,0 +1,20 @@
+﻿int b = Random.Shared.Next(1, 5);
+//var b = a;
+switch (b)
+{
+	case 1:
+		Console.WriteLine($"got 1...{b}");
+		goto case 2;
+	case 2:
+        Console.WriteLine($"landed at 2: {b}");
+		break;
+	case 3:
+        Console.WriteLine($"3s a crowd: {b}");
+		break;
+    case 5:
+        Console.WriteLine($"5 d: {b}");
+        break;
+	default:
+        Console.WriteLine($"defaults to 4: {b}");
+		break;
+}
