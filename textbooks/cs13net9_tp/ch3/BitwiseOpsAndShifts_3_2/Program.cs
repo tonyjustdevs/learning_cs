a29a58594bdbcf029adc29225180b5ecfba815a6
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-08 12:33:41 +0700

    add BitwiseOpsAndShifts_3_2.sln 081025

---
+++ textbooks/cs13net9_tp/ch3/BitwiseOpsAndShifts_3_2/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = 10;
+int y = 6;
+
+string[] headings = { "vars", "decimal", "binary","expected" };
+char[] vars = { 'x', 'y' };
+
+Console.WriteLine($"{headings[0],5}|{headings[1],8}|{headings[2],8}|{headings[3],9}");
+Console.WriteLine($"{'x',5}|{      x,8}|{  x,8:B}|{"1010",9}");            
+Console.WriteLine($"{'y',5}|{      y,8}|{  y,8:B}|{"110",9}");            
+
+Console.WriteLine($"{("x&y"),5}|{x&y,8}|{x&y,8:B}|{"10",9}");    
+
+Console.WriteLine($"{("x|y"),5}|{x|y,8}|{x|y,8:B}|{"1110",9}");    
+
+Console.WriteLine($"{("x^y"),5}|{x^y,8}|{x^y,8:B}|{"1100",9}");    
+
+//vars | decimal | binary
+//    x | 10 | 1010
+    //y | 6 | 110
---
+++ textbooks/cs13net9_tp/ch3/SwitchingAnimals_3_2/Program.cs
@@ -0,0 +1,47 @@
+﻿Cat oreo = new Cat { Name = "oreo", Legs = 4, DOB = new DateOnly(2018, 12, 1) ,isDomesticated=false};
+Cat lilo= new Cat { Name = "lilo", Legs = 4, DOB = new DateOnly(2018, 12, 1) ,isDomesticated=true};
+Snake snek = new Snake{ Name = "sneko",Legs= 0, DOB=new DateOnly(2019, 4, 30), isVenomous=false};
+
+var animal_farm = new List<Animal?> { oreo, lilo, null, snek };
+
+foreach (Animal? animal in animal_farm)
+{
+    //if (animal is null) { continue; }
+    //Console.WriteLine($"{animal?.Name} is a {animal?.GetType()} with {animal?.Legs} legs & DOB {animal?.DOB}"); 
+    string msg;
+    switch (animal)
+    {
+        case Cat FourLeggedFreak when FourLeggedFreak.Legs == 4 && FourLeggedFreak.isDomesticated == true:
+            msg = $"[{nameof(FourLeggedFreak)}]: you four-legged freaky docile nice kitty, {FourLeggedFreak.Name}."; 
+            break;
+        case Cat WildFeline when WildFeline.isDomesticated == false && WildFeline.Legs == 4:
+            msg = $"[{nameof(WildFeline)}]: damn {WildFeline.Name} you are wild one!";
+            break;
+        case Snake:
+            msg = $"why such a normal snake, Snake?";
+            break;
+        default:
+            msg = "a regular animal construct";
+            break;
+        case null:
+            msg = "a non-existent animal!";
+            break;
+    }
+    Console.WriteLine(msg);
+}
+class Animal
+{
+    public required string Name;
+    public int Legs;
+    public DateOnly DOB;
+}
+
+class Cat : Animal
+{
+    public bool isDomesticated;
+}
+
+class Snake : Animal
+{
+    public bool isVenomous;
+}
