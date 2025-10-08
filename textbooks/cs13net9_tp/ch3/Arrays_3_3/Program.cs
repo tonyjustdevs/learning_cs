1943e51a2ffd659e1fcd5a8fc3fe36787df800a7
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-08 17:17:39 +0700

    add Arrays_3_3.sln 081025

---
+++ textbooks/cs13net9_tp/ch3/Arrays_3_3/Program.cs
@@ -0,0 +1,41 @@
+﻿
+string[] names;
+names               = new string[4];
+names[0] = "aaa";
+names[1] = "bbb";
+names[3] = "ccc";
+
+string[] names2     = new string[] { "mate", "ange", "cuz", "yolo" };
+var names3  = new string[] { "aaaa", "bbbb", "cccc", "dddd" };
+var names4 = new[] { "aa", "bb", "cc", "dd" };
+string[] names5 = [ "aaaa", "bbbbb", "ccccc", "ddddd" ];
+
+
+foreach (var item in names)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+
+foreach (var item in names2)
+{
+    Console.WriteLine(item);
+
+}
+Console.WriteLine();
+foreach (var item in names3)
+{
+    Console.WriteLine(item);
+
+}
+Console.WriteLine();
+foreach (var item in names4)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine();
+foreach (var item in names5)
+{
+    Console.WriteLine(item);
+}
