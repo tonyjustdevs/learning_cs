993e765c6df445c2aa81182d6ae9e66256d11164
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-08 17:24:44 +0700

    add GridBounds_3_3.sln 081025

---
+++ textbooks/cs13net9_tp/ch3/GridBounds_3_3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// 1-dim arr
+
+string[] names = new string[] { "aaaa", "bbbb", "cccc" };
+
+
+// 2-d arr
+string[,] names_grid = new string[,] {
+    { "aaaa", "bbbb", "cccc"},
+    { "aa", "bb", "cc"},
+    { "aaa", "bbb", "ccc"},
+    { "aaasda", "bbasdfb", "ccasdfc"},
+};
+
+
+Console.WriteLine($"names_grid.GetLowerBound(0): {names_grid.GetLowerBound(0)} (EXP: 0)");
+Console.WriteLine($"names_grid.GetUpperBound(0): {names_grid.GetUpperBound(0)} (EXP: 3)");
+
+Console.WriteLine($"names_grid.GetLowerBound(1): {names_grid.GetLowerBound(1)} (EXP: 0)");
+Console.WriteLine($"names_grid.GetUpperBound(1): {names_grid.GetUpperBound(1)} (EXP: 2)");
