e4f19877aaa968ba066e8bc173a7df11eb198b1e
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-08 20:41:31 +0700

    add Casting_3_5.sln 081025

---
+++ textbooks/cs13net9_tp/ch3/Casting_3_5/Program.cs
@@ -0,0 +1,14 @@
+﻿double c = 9.8;
+
+int d = (int)c; // Compiler gives an error if you do not explicitly cast.
+//int d = c; // Compiler gives an error if you do not explicitly cast.
+Console.WriteLine($"c is {c}, d is {d}"); // d loses the .8 part.
+
+MyClass myClass = new();
+
+object obj = myClass;
+
+class MyClass()
+{
+    string a;
+}
