e03ce899f84274f105eafe3e407b22dfc1a980c5
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-10 01:11:22 +0700

    add TypesToString_3_5.sln 101025

---
+++ textbooks/cs13net9_tp/ch3/TypesToString_3_5/Program.cs
@@ -0,0 +1,15 @@
+﻿int x = 69; // int
+bool t = true;// bool
+//DateTime dt = new DateTime();
+DateTime dt = DateTime.Now;
+
+object obj = new();
+// datetime 
+
+// object
+
+
+Console.WriteLine(x.ToString());
+Console.WriteLine(t.ToString());
+Console.WriteLine(dt.ToString());
+Console.WriteLine(obj.ToString());
