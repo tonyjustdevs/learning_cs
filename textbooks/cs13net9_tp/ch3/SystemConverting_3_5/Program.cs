4d0a7ae78629f292bb2cf155bd2050c8673c1507
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-09 22:09:54 +0700

    add SystemConverting_3_5.sln 091025

---
+++ textbooks/cs13net9_tp/ch3/SystemConverting_3_5/Program.cs
@@ -0,0 +1,11 @@
+﻿// 0. import
+using static System.Convert;
+
+// 1. declare
+double g_dbl = 9.8;
+// 2. convert
+int g_int = ToInt32(g_dbl);
+
+// 3. print
+Console.WriteLine($"g_dbl: {g_dbl}");
+Console.WriteLine($"g_int: {g_int} (via ToInt32(g)");
