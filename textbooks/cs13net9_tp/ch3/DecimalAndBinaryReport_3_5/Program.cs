9107d7063621ce44cf6ba0dab335be3ea63d7ce3
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-09 21:18:48 +0700

    add DecimalAndBinaryReport_3_5.sln 091025

---
+++ textbooks/cs13net9_tp/ch3/DecimalAndBinaryReport_3_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// headings
+Console.WriteLine("{0,12}{1,34}","Decimal","Binary");
+
+Console.WriteLine("{0,12}{0,34:B32}", int.MaxValue);
+
+for (int i = 10; i >=-10; --i)
+{
+    Console.WriteLine("{0,12}{0,34:B32}", i);
+}
+
+
+
+
+Console.WriteLine("{0,12}{0,34:B32}" ,int.MinValue);
+
+
---
+++ textbooks/cs13net9_tp/ch3/Truncation_3_5/Program.cs
@@ -0,0 +1,9 @@
+﻿long r = 0b_101000101010001100100111010100101010;
+int s = (int)r;
+//long t1 = 0b_00101010001100100111010100101010;
+//int t2 = (int)t1;
+
+Console.WriteLine($"r{r,50:B50} = {r}");
+Console.WriteLine($"s{s,50:B50} = {s}");
+//Console.WriteLine($"t1{t1,38:B32} = {t1}");
+//Console.WriteLine($"t2{t2,38:B32} = {t2}");
