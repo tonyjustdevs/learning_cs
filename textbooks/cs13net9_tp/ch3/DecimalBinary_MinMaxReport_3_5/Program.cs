262620a5ff79944ffe258ab5fcdc6ff463407a76
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-09 20:15:41 +0700

    add DecimalBinary_MinMaxReport_3_5.sln 091025

---
+++ textbooks/cs13net9_tp/ch3/DecimalBinary_MinMaxReport_3_5/Program.cs
@@ -0,0 +1,17 @@
+﻿//headings
+const int dec1 = 12;
+const int bin1 = 34;
+const string bin2 = "34:B32";
+Console.WriteLine($"{"Decimal",dec1}{"Binary",bin1}");
+Console.WriteLine("{0,12}{0,34:B32}",int.MaxValue   );
+// int.max
+//string formatted = string.Format("{0," + dec1 + "}{0," + bin2 + "}", int.MaxValue); //{0,12}{0,34:B32}
+//
+for (int i = -10; i <= 10; i++)
+{
+    Console.WriteLine($"{ i,12}{ i,34:B32}");
+}
+
+Console.WriteLine("{0,12}{0,34:B32}", int.MinValue+2);
+Console.WriteLine("{0,12}{0,34:B32}", int.MinValue+1);
+Console.WriteLine("{0,12}{0,34:B32}", int.MinValue-0);
