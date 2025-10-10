72f48af9fd599f49d8218e0bef1e95768a619de2
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-10 15:34:15 +0700

    add CatchingWithFilters_3_6.sln 101025

---
+++ textbooks/cs13net9_tp/ch3/CatchingWithFilters_3_6/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Enter an amount: ");
+
+string? amt_str = Console.ReadLine();
+
+if (string.IsNullOrEmpty(amt_str)){ return; }
+
+try
+{
+    int amt_int = int.Parse(amt_str);
+    Console.WriteLine($"you entered {amt_int}");
+}
+
+catch (FormatException fe) when (amt_str.Contains("$"))
+{
+    Console.WriteLine($"Bro you cant type in $! {amt_str} [{fe.GetType()}] [{fe.Message}]");
+    throw;
+}
+catch (FormatException fe)
+{
+    Console.WriteLine($"are you illiterate! {amt_str} [{fe.GetType()}] [{fe.Message}]");
+	throw;
+}
