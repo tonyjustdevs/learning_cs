c878b1529d2257120435e732b5ece1da962183bb
tonyjustdevs <tonyjustdevs@gmail.com>
2025-10-10 18:15:02 +0700

    add CheckedOverflow_3_7D.sln 101025

---
+++ textbooks/cs13net9_tp/ch3/CheckedOverflow_3_7/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Console.WriteLine($"{"Integer Max Value",18}   {"Increment",10}   {"Value",15}");
+//for (int i = -2; i < 5; i++)
+//{
+//    Console.WriteLine($"{int.MaxValue,18} + {i,10} = {int.MaxValue+i,15}");
+//}
+
+//Console.WriteLine();
+
+
+//Console.WriteLine($"{"Integer Min Value",18}   {"Increment",10}   {"Value",15}");
+//for (int i = 2; i >= -5; i--)
+//{
+//    Console.WriteLine($"{int.MinValue,18} + {i,10} = {int.MinValue + i,15}");
+//}
+
+//Console.WriteLine("now with checked: ");
+
+checked { 
+    Console.WriteLine($"{"Integer Max Value",18}   {"Increment",10}   {"Value",15}");
+    for (int i = -2; i < 5; i++)
+    {
+        Console.WriteLine($"{int.MaxValue,18} + {i,10} = {int.MaxValue + i,15}");
+    }
+}//Unhandled exception. System.OverflowException: Arithmetic operation resulted in an overflow.
+
+Console.WriteLine();
+
+checked { 
+    Console.WriteLine($"{"Integer Min Value",18}   {"Increment",10}   {"Value",15}");
+    for (int i = 2; i >= -5; i--)
+    {
+        Console.WriteLine($"{int.MinValue,18} + {i,10} = {int.MinValue + i,15}");
+    }
+}
