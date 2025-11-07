b5e0808b8350aa39d0129a1a40d6ffbbb4d6fbe8
tonyjustdevs <tonyjustdevs@gmail.com>
2025-11-07 23:26:39 +0700

    add event class  071125

---
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/ClassLibrary1/Player2.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+
+namespace TP.SharedLibraries;
+
+public class Player2
+{
+    public int Points { get; private set; }
+    public delegate void Achieved100PointsHandler(int points);
+
+    //public event Action? Achieved100Points;
+    public event Achieved100PointsHandler? Achieved100Points;
+    public async Task AddPoints(int points)
+    {
+        Points += points;
+        Console.WriteLine($"{points} p  oints added! Total Points: {Points}");
+        await Task.Delay(150);
+
+        if (Points > 100) {
+            Achieved100Points?.Invoke(Points);
+            //Console.WriteLine($"Congratulations, you've reached {Points} points");
+        }
+    }
+}
+// [creating and using event]
+// 1.  add eventfld action
+// 2a. add eventfld?.invoke() somewhere you want it to be invoked (so people can subscribe to it)
+// 2b.   e.g. add insde a method() with if() criteria to reach
+// 3. in program.cs  add custom_methods() to the field, so these custom_methods() will run if criteria reached.
---
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/PointsApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using TP.SharedLibraries;
+Console.WriteLine("Welcome to Points App 2");
+
+Player2 p = new();
+p.Achieved100Points += some_subbed_fn;
+await p.AddPoints(10);
+await p.AddPoints(20);
+await p.AddPoints(40);
+await p.AddPoints(80);
+
+
+void some_subbed_fn(int points)
+{
+    Console.WriteLine($"Congratulations from Program.cs, you've reached over {points} points");
+
+}
