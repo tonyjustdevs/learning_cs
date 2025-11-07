9e8c9f456d9592f0886f4122fb83947b6f106cd6
tonyjustdevs <tonyjustdevs@gmail.com>
2025-11-07 15:30:28 +0700

    add psuedo-code player cs & async await events method 071125

---
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/PointsApp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+Console.WriteLine("Welcome to Points App!");
+
+// [A] add [Player.cs]
+// 0.  add add to Namespace
+// 1.  add Player cls
+// 2.  add Points fld {getter/setter}
+// 3a. add async Task AddPoints method(): param int points
+// 3b.  add Pts += pts
+// 3c.  add CW => pts & Pts
+// 3d.  add await Task.Delay(100);
+// 3e.  add if => Pts>100 => CW congrats
+
+
+// [B] add [Program.cs]
+// 1.  Create Player instance
+// 2.  await p.AddPoints() 
