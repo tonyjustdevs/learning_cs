51db1bef2a309b2ef32b0b3a9721c8b33e7cd135
tonyjustdevs <tonyjustdevs@gmail.com>
2025-11-22 18:29:32 +0700

    add HasValue methods 221125

--- textbooks/cs13net9_tp/ch6/Ch6Soln/NullHandling/Program.cs
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/NullHandling/Program.cs
@@ -8,19 +8,23 @@
 
 // 3. comment out 2.
 // 4. add nullable int = null;
-int? some_int_nullable = null;
+int? nullable_int = null;
 
 // 5. print - ok
-Console.WriteLine(some_int_nullable);                       // ""
-Console.WriteLine(some_int_nullable.GetValueOrDefault());   // 0 
+Console.WriteLine($"nullable_int: {nullable_int}");                       // ""
+Console.WriteLine($"nullable_int.HasValue: {nullable_int.HasValue}");                       // ""
+Console.WriteLine($"nullable_int.GetValueOrDefault(): {nullable_int.GetValueOrDefault()}");   // 0 
 
 // 6. assign nullable int = 7;
-some_int_nullable = 7;
-Console.WriteLine(some_int_nullable);                       // 7
-Console.WriteLine(some_int_nullable.GetValueOrDefault());   // 7
+nullable_int = 7;
+Console.WriteLine($"nullable_int = 7: {nullable_int}");                       // ""
+Console.WriteLine($"nullable_int.HasValue: {nullable_int.HasValue}");                       // ""
+Console.WriteLine(nullable_int.GetValueOrDefault());   // 7
 
 // 7. alternative syntax of null int
 Nullable<int> AnotherNullableInt = 69;
-Console.WriteLine($"pre-null: {AnotherNullableInt }");
+Console.WriteLine($"pre-null: {AnotherNullableInt}");
+Console.WriteLine($"pre-null.HasValue: {AnotherNullableInt.HasValue}");
 AnotherNullableInt = null;
 Console.WriteLine($"post-null: {AnotherNullableInt }");
+Console.WriteLine($"pre-null.HasValue: {AnotherNullableInt.HasValue}");
