ba8528ad7ad9203276235de2c15753b9869be6dd
tonyjustdevs <tonyjustdevs@gmail.com>
2025-11-22 18:11:38 +0700

    add NullHandling proj & nullable & non-nullable int instances 221125

---
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/NullHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// 1. add non-null int
+//int some_int_nonnullable = null; // must comment out
+//ok -"Cannot convert null to 'int' because it is a non-nullable value type"
+
+// 2. print - error
+//Console.WriteLine(some_int_nonnullable); // ok error: "Cannot convert null to 'int' because it is a non-nullable value type"
+
+// 3. comment out 2.
+// 4. add nullable int = null;
+int? some_int_nullable = null;
+
+// 5. print - ok
+Console.WriteLine(some_int_nullable);                       // ""
+Console.WriteLine(some_int_nullable.GetValueOrDefault());   // 0 
+
+// 6. assign nullable int = 7;
+some_int_nullable = 7;
+Console.WriteLine(some_int_nullable);                       // 7
+Console.WriteLine(some_int_nullable.GetValueOrDefault());   // 7
+
