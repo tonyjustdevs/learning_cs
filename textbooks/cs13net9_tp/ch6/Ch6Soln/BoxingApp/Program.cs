0569fe8f90372880ae0485ae5e92f0fd4459f8f4
tonyjustdevs <tonyjustdevs@gmail.com>
2025-12-03 17:03:25 +0700

    update ch6_memory.ipynb 031225

--- textbooks/cs13net9_tp/ch6/Ch6Soln/BoxingApp/Program.cs
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/BoxingApp/Program.cs
@@ -12,3 +12,38 @@ Console.WriteLine($"int unboxed_a = (int) a = {unboxed_a}");
 
 // 2/12/25 continue ch06-memory.md before chapter 7
 // https://github.com/markjprice/cs14net10/blob/main/docs/ch06-memory.md
+
+
+// boxing app v2
+
+char some_char = 'a';
+object boxed_char = some_char;
+char unboxed_char = (char)boxed_char;
+
+
+int cool_int = 5;
+System.Int32 cool_int2 = 6;
+
+Console.WriteLine($"[int] cool_int.GetType(): {cool_int.GetType()}");
+Console.WriteLine($"[System.Int32] cool_int2.GetType(): {cool_int2.GetType()}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
