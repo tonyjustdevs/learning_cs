237b7844ef78fa061eb968e924a93a6fdd594573
tonyjustdevs <tonyjustdevs@gmail.com>
2025-12-03 19:10:18 +0700

    add struct operator additiona & equality overrides 031225

---
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/ClassLibrary1/DisplacementVector.cs
@@ -0,0 +1,44 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TP.SharedLibraries;
+
+public struct DisplacementVector
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public DisplacementVector(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public static DisplacementVector operator+(DisplacementVector dv_1, DisplacementVector dv_2)
+    {
+        int new_x = dv_1.X + dv_2.X;
+        int new_y = dv_1.Y + dv_2.Y;
+        return new DisplacementVector(new_x, new_y);
+    }
+    
+    public static bool operator==(DisplacementVector dv1, DisplacementVector dv2)
+    {
+        Console.WriteLine("running custom overriden equality '=='");
+        return dv1.Equals(dv2);
+    }
+    public static bool operator !=(DisplacementVector dv1, DisplacementVector dv2)
+    {
+        Console.WriteLine("running custom overriden inequality '!='");
+        return !dv1.Equals(dv2);
+    }
+
+}
+
+// 1.  declared type: struct (instead of class.)
+// 2.  add [two int properties], named:
+//      - 'X' and "Y",
+//      - Auto-generate [two] [private fields] with the same data type,
+//      - Which will be allocated on the [stack].
+// 3.  add [constructor] to:
+//      - set initial values for X and Y.
+// 4.  add [operator ] to
+//      - [add] two instances together that
+//      - [returns] a new instance of the type, with X added to X, and Y added to Y
---
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/MemoryProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using TP.SharedLibraries;
+Console.WriteLine("Hello, Memory Program!");
+
+DisplacementVector dv1 = new( 3,5);
+DisplacementVector dv2 = new(-2,7);
+
+Console.WriteLine($"dv1.X[{dv1.X}]+dv2.X[{dv2.X}] = dv1.X+dv2.X[{dv1.X+dv2.X}]");
+Console.WriteLine($"dv1.Y[{dv1.Y}]+dv2.Y[{dv2.Y}] = dv1.Y+dv2.Y[{dv1.Y + dv2.Y}]");
+
+Console.WriteLine("Default Values");    
+DisplacementVector dv3 = new();
+
+Console.WriteLine($"dv3.X: {dv3.X}");
+Console.WriteLine($"dv3.Y: {dv3.Y}");
+
+DisplacementVector dv2_v2 = new(-2, 7);
+// compare dv2 vs dv2_v2  
+Console.WriteLine($"dv2.Equals(dv2_v2): {dv2.Equals(dv2_v2)}");
+Console.WriteLine($"dv2==dv2_v2: {dv2==dv2_v2}");
+Console.WriteLine($"dv2!=dv1: {dv2!=dv1}");
