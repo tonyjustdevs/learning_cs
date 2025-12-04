f5226c1941758dec5cea6fb1f0beb467ea99f770
tonyjustdevs <tonyjustdevs@gmail.com>
2025-12-04 18:10:24 +0700

    add EarthPosition record cls 041225

---
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/ClassLibrary1/EarthPosition.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace TP.SharedLibraries;
+
+public record EarthPosition
+{
+    public int Longitude { get; set; }
+    public int Latitude { get; set; }
+
+    public EarthPosition() { }
+    public EarthPosition(int longitude, int latitude)
+    {
+        Longitude = longitude;
+        Latitude = latitude;
+    }
+
+    public static EarthPosition operator+(EarthPosition ep1, EarthPosition ep2)
+    {
+        return new EarthPosition(ep1.Longitude + ep2.Longitude, ep1.Latitude + ep2.Latitude);
+    }
+
+    //public static bool operator==(EarthPosition ep1, EarthPosition ep2)
+    //{
+
+    //    return (ep1.Longitude == ep2.Longitude && ep1.Latitude == ep2.Latitude) ? true : false;
+    //}
+    //public static bool operator !=(EarthPosition ep1, EarthPosition ep2)
+    //{
+
+    //    return (ep1.Longitude == ep2.Longitude && ep1.Latitude == ep2.Latitude) ? true : false;
+    //}
+
+    #region OutputMethods
+    public override string ToString()
+    {
+        return $"({Longitude},{Latitude})";
+    }
+    #endregion
+
+}
---
+++ textbooks/cs13net9_tp/ch6/Ch6Soln/EarthProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using TP.SharedLibraries;
+
+Console.WriteLine("Welcome to Earth Program!");
+
+EarthPosition ep1 = new EarthPosition() { Longitude = 69, Latitude=420};
+EarthPosition ep2 = new EarthPosition() { Longitude = 69, Latitude=420};
+EarthPosition ep3 = new EarthPosition() { Longitude = 555, Latitude=666};
+
+Console.WriteLine(ep1.ToString());
+Console.WriteLine(ep2.ToString());
+Console.WriteLine(ep3.ToString());
+
+Console.WriteLine("\nCompare via '=':");
+Console.WriteLine($"ep1==ep2: {ep1 == ep2}");
+Console.WriteLine($"ep1==ep3: {ep1 == ep3}");
+
+Console.WriteLine("\nCompare via 'Equals()':");
+Console.WriteLine($"ep1.Equals(ep2): {ep1.Equals(ep2)}");
+Console.WriteLine($"ep1.Equals(ep3): {ep1.Equals(ep3)}");
