6f24575204ee67b93c3461126d0c5870e727ce3a
tonyjustdevs <tonyjustdevs@gmail.com>
2025-12-11 15:03:38 +0700

    add 3 methods to build efcore models 111225

---
+++ textbooks/cs14net10/ch10/Ch10Soln/Ch10sln/WorkingWithEFCore/NorthwindDb2.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Northwind.EntityModels;
+
+public class NorthwindDb2 : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder options_builder)
+    {
+        Console.WriteLine($"Entered OnConfiguring()...\n");
+        string file_name = "Northwind.db";
+        string path = Path.Combine(Environment.CurrentDirectory, file_name);
+        string conn_string = $"Data Source={path}";
+
+        Console.WriteLine($"conn_string: {conn_string}\n");
+        options_builder.UseSqlite(conn_string);
+    }
+}
--- textbooks/cs14net10/ch10/Ch10Soln/Ch10sln/WorkingWithEFCore/Program.cs
+++ textbooks/cs14net10/ch10/Ch10Soln/Ch10sln/WorkingWithEFCore/Program.cs
@@ -6,10 +6,15 @@ Console.WriteLine("Hello, WorkingWithEFCore!\n");
 
 Console.WriteLine("Creating DB Instance\n");
 
-NorthwindDb nwdb = new NorthwindDb();
+//NorthwindDb nwdb = new NorthwindDb();
 //nwdb.doOnConfig();
-Console.WriteLine($"nwdb.Database.ProviderName: {nwdb.Database.ProviderName}\n");
+//Console.WriteLine($"nwdb.Database.ProviderName: {nwdb.Database.ProviderName}\n");
 
+
+
+
+NorthwindDb2 nw_db2 = new();
+Console.WriteLine($"nw_db2.Database.ProviderName(): {nw_db2.Database.ProviderName}");
 
 // 0.includ pkg, add new class, using efc, add namspace
 // 1a. pc nwdb : dbcontext cls
