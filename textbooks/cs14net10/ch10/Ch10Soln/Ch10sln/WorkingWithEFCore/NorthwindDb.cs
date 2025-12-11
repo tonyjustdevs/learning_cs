14d801647fd65c988ec242a96ed90f95697f4b1d
tonyjustdevs <tonyjustdevs@gmail.com>
2025-12-11 13:01:02 +0700

    add Northwind cls and custom OnConfiguring method 111225

---
+++ textbooks/cs14net10/ch10/Ch10Soln/Ch10sln/WorkingWithEFCore/NorthwindDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Northwind.EntityModels;
+
+public class NorthwindDb:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //base.OnConfiguring(optionsBuilder)
+        //
+        string db_path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+        string connection_string = $"Data Source={db_path}";
+        
+        Console.WriteLine($"db_path: {db_path}\n");
+
+        Console.WriteLine($"connection_string: {connection_string}\n");
+        optionsBuilder.UseSqlite(connection_string);
+    }
+
+
+
+}
+
+
+// Unhandled exception. System.InvalidOperationException:
+// No database provider has been configured for this DbContext.
+// A provider can be configured by overriding the 'DbContext.OnConfiguring' method
+// or by using 'AddDbContext' on the application service provider.
+// If 'AddDbContext' is used, then also ensure that your DbContext type accepts a DbContextOptions<TContext> object
+// in its constructor and passes it to the base constructor for DbContext.
--- textbooks/cs14net10/ch10/Ch10Soln/Ch10sln/WorkingWithEFCore/Program.cs
+++ textbooks/cs14net10/ch10/Ch10Soln/Ch10sln/WorkingWithEFCore/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Northwind.EntityModels;
+
+Console.WriteLine("Hello, WorkingWithEFCore!\n");
+
+
+Console.WriteLine("Creating DB Instance\n");
+
+NorthwindDb nwdb = new NorthwindDb();
+//nwdb.doOnConfig();
+Console.WriteLine($"nwdb.Database.ProviderName: {nwdb.Database.ProviderName}\n");
+
+
+// 0.includ pkg, add new class, using efc, add namspace
+// 1a. pc nwdb : dbcontext cls
+// 1b. pov onconfiguring(OB ob)
+// 2. combinate path
+// - create path
+// - file name
+// 3. create conencetion string connection${...}
+// 4.optionsBuilder.sqlite(conn_str)
