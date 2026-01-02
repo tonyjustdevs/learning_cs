e82f4bb128ace0eedc53d2b3f46c64482b4ed4ae
tonyjustdevs <tonyjustdevs@gmail.com>
2026-01-02 12:45:06 +0700

    add draft set configcultures

---
+++ textbooks/cs13net9_tp/ch5/Ch5Soln23Oct25/Ch5OOPSoln/CarApp/Program.Helper.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+partial class Program
+{
+    // add static method
+    public static void ConfigCulture()
+    {
+
+        var mate    = CultureInfo.CurrentCulture; //en - AU
+        var getinfo = CultureInfo.GetCultureInfo("en-AU");
+
+        if (mate.Name == getinfo.Name || mate.Equals(getinfo))
+        {
+            Console.WriteLine($"same data, but not nec same object: '{mate}' '{getinfo}'");
+        } else
+        {
+            Console.WriteLine($"not same: '{mate}' '{getinfo}'");
+        }
+
+     
+
+        Console.WriteLine("CurrentCulture: [" + mate + "] [rocket: 🚀]");
+        Console.WriteLine("GetCultureInfo('en-AU'): " + getinfo + " [rocket: 🚀]");
+        
+        
+        System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        
+        //CultureInfo.CurrentCulture = new CultureInfo("en-US");
+        getinfo = CultureInfo.GetCultureInfo("en-US");
+        mate    = CultureInfo.CurrentCulture;
+        
+        
+        Console.WriteLine("CurrentCulture: [" + mate + "] [rocket: 🚀]");
+        Console.WriteLine("GetCultureInfo('en-US'): " + getinfo + " [rocket: 🚀]");
+
+
+        //CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+    }
+    // add culture class
+
+}
--- textbooks/cs13net9_tp/ch5/Ch5Soln23Oct25/Ch5OOPSoln/CarApp/Program.cs
+++ textbooks/cs13net9_tp/ch5/Ch5Soln23Oct25/Ch5OOPSoln/CarApp/Program.cs
@@ -9,3 +9,9 @@ Console.WriteLine("Hello To Car App!");
                                         // - Program.cs is a seperate class/assembly 
 Car camry= new(wheels: 4, isEV: true) { }; // [C20122 inaccessible error]: Car cls is [internal]
 camry.Start();
+
+
+
+//ConfigCulture()
+
+ConfigCulture();
