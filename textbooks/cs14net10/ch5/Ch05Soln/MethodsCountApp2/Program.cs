11a4c351eb6bce8213b324874fa226f59505994a
tonyjustdevs <tonyjustdevs@gmail.com>
2026-01-03 15:46:45 +1100

    add unused Dataset & Httpclient instances declarations

--- textbooks/cs14net10/ch5/Ch05Soln/MethodsCountApp2/Program.cs
+++ textbooks/cs14net10/ch5/Ch05Soln/MethodsCountApp2/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 Console.WriteLine("Hello to MethCounter2");
 
+System.Data.DataSet ds = new();
+HttpClient client = new();
 
 // 1. get entry pt to assembly
 Assembly? assembly = Assembly.GetEntryAssembly();
@@ -10,9 +13,9 @@ if (assembly == null) { return; }
 foreach (var ref_assembly_object in assembly.GetReferencedAssemblies())
 {
     //Console.WriteLine($"ref_assembly_object.GetType(): {ref_assembly_object.GetType()}");
-// 3. load each assembly
+    // 3. load each assembly
     var loaded_a = Assembly.Load(ref_assembly_object);
-    Console.WriteLine($"\nloaded_a: {ref_assembly_object.Name}");
+    //Console.WriteLine($"\nloaded_a: {ref_assembly_object.Name}");
 
     // 4. get types
     // 4i. count types
@@ -27,14 +30,12 @@ foreach (var ref_assembly_object in assembly.GetReferencedAssemblies())
 
     }
     Console.WriteLine($"[{no_of_types}] types has [{no_of_meths}] methods in [{ref_assembly_object.Name}]");
-
-    //loaded_a.DefinedTypes.ToList().ForEach( t => 
-    //{
-    //    var methods = t.GetMethods();
-    //    Console.WriteLine($"Type: {t}, Methods Count: {methods.Length}");
-    //});
+    //[0] types has[0] methods in [System.Runtime]
+    //[413] types has[7194] methods in [System.Data.Common]
+    //[435] types has[4772] methods in [System.Net.Http]
+    //[44] types has[656] methods in [System.Console]
+    //[119] types has[1491] methods in [System.Linq]
 }
-
 
 
 // 6. tally methods
