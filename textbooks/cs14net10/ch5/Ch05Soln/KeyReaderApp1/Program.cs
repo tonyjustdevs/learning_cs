540afc3d1381536597b986049b6b2daf19c49b4b
tonyjustdevs <tonyjustdevs@gmail.com>
2026-01-05 15:21:27 +1100

    add KeyReaderApp1

---
+++ textbooks/cs14net10/ch5/Ch05Soln/KeyReaderApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Hello KeyReadApp1!");
+
+var key = Console.ReadKey();
+
+Console.WriteLine("Key: {0}, KeyChar: {1}, Modifiers:{2}",key.Key, key.KeyChar,key.Modifiers);
+//var key2 = Console.Read();
