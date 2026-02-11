71d51d8461282de852dcadf0380b57e1d86026dd
tonyjustdevs <tonyjustdevs@gmail.com>
2026-02-12 00:01:40 +0700

    add Shape abstract class and inherited classes

---
+++ textbooks/cs14net10/ch6/20260211/Ch6Soln/TPSharedModernLib/Shape.cs
@@ -0,0 +1,45 @@
+﻿namespace TPNS.TPSharedModernLib;
+public abstract class Shape
+{ //2. Create a class named Shape: properties named Height, Width, and Area.
+    protected double Height { get; set; }
+    protected double Width { get; set; }
+    protected virtual double Area { get; }
+}
+
+
+// 3. Rectangle, Square, and Circle
+// - any additional members: override Area property if appropriate.
+
+public class Rectangle : Shape
+{
+    //Rectangle r = new(height: 3, width: 4.5);
+    public Rectangle() { }
+
+    public Rectangle(double height, double width)
+    {
+        Height  = height;
+        Width   = width;
+    }
+    protected override double Area => Height * Width;
+}
+
+public class Square : Rectangle
+{// Square s = new(5);
+    public Square() { }
+
+    public Square(double height) : base(height, height) { }
+
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; }
+    public Circle() { }
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public new double Area => Math.PI * (Radius * Radius);
+
+}
---
+++ textbooks/cs14net10/ch6/20260211/Ch6Soln/TPSharedModernLib/Shape2.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Text;
+
+namespace TPNS.TPSharedModernLib;
+
+public abstract class Shape2
+{
+    protected double Width { get; set; }
+    protected double Height { get; set; }
+    protected virtual double Area { get; }
+}
+
+public class Rectangle2 : Shape2
+{
+    public Rectangle2(){}
+    public Rectangle2(double height, double width)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    protected override double Area => Height * Width;
+}
+
+public class Square2 : Rectangle2 
+{
+    public Square2(){}
+    public Square2(double width) : base(width, width){} // takes only one input
+}
+public class Circle2 : Square2
+{
+    public Circle2() { }
+    public Circle2(double radius) : base(radius) { } // takes only one input
+    public override double Area => Math.PI * Height * Width;
+
+}
+
+//Rectangle r = new(height: 3, width: 4.5);
+//WriteLine($"Rectangle H: {r.Height}, W: {r.Width}, Area: {r.Area}");
+//    Square s = new(5);
+//    WriteLine($"Square H: {s.Height}, W: {s.Width}, Area: {s.Area}");
+//    Circle c = new(radius: 2.5);
+//    WriteLine($"Circle H: {c.Height}, W: {c.Width}, Area: {c.Area}");
+
+//Rectangle H: 3, W: 4.5, Area: 13.5
+//Square H: 5, W: 5, Area: 25
+//Circle H: 5, W: 5, Area: 19.6349540849362
+
+
