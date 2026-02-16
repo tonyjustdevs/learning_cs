f01b96ac053ce53fd640888ad47ce87f6514f121
tonyjustdevs <tonyjustdevs@gmail.com>
2026-02-16 17:32:42 +0700

    add xml creator attempt 1

---
+++ textbooks/cs14net10/ch9/160226/Ch9Soln/XmlExporterApp/Program.Helpers.cs
@@ -0,0 +1,18 @@
+﻿
+using TPNS.SharedLibrary;
+
+partial class Program
+{
+    private static void OutputFileInfo(string txt_file_path)
+    {
+        var viper_read_txt = File.ReadAllText(txt_file_path);
+
+        WriteLine($"File Name: {GetFileName(txt_file_path)}");
+        WriteLine($"Dir Name: {GetDirectoryName(txt_file_path)}");
+        WriteLine($"File Size: {new FileInfo(txt_file_path).Length:N0}");
+        WriteLine($"File Size: {new FileInfo(txt_file_path):N0}");
+        WriteLine($"Contents:\n/------------------------\n");
+        WriteLine(viper_read_txt);
+        WriteLine($"------------------------/");
+       }
+}
---
+++ textbooks/cs14net10/ch9/160226/Ch9Soln/XmlExporterApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml;
+using TPNS.SharedLibrary;
+Console.WriteLine("Welcome to XMLExporterApp");
+
+// add file path
+var viper_file_path = Combine(CurrentDirectory, "viper.xml");
+
+//StreamReader? stream=null;
+XmlWriter? xml = null;
+FileStream? viper_stream = null;
+//using var xml = XmlWriter.Create(viper_file_path, new XmlWriterSettings() { Indent=true});
+viper_stream =File.Create(viper_file_path);
+xml = XmlWriter.Create(viper_stream, new XmlWriterSettings {Indent=true});
+//xmlFileStream = File.Create(xmlFile);
+xml.WriteStartDocument();
+
+xml.WriteStartElement("LEVEL1");
+
+
+xml.WriteElementString("LEVEL2","bro1");
+xml.WriteStartElement("LEVEL3");
+xml.WriteElementString("LEVEL3-1", "YEAH3");
+xml.WriteElementString("LEVEL3-1", "YEAH3");
+xml.WriteEndElement();
+xml.WriteElementString("LEVEL2", "bro2");
+xml.WriteElementString("LEVEL2", "bro3");
+xml.WriteEndElement();
+//xml.WriteEndElement();
+
+xml.Close();
+viper_stream.Close();
+OutputFileInfo(viper_file_path);
+//stream = File.OpenText(viper_file_path);
+////stream()
+//xml.GetHashCode()
+//// read viper class
+//foreach (var viper in Viper.CallSigns)
+//{
+//    WriteLine($"{viper}");
+//}
+//// add filestrem?
+//// add xml stream?
+
