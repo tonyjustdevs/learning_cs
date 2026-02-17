1f4bd4273ba25e7e1529d37bb519549b418e6716
tonyjustdevs <tonyjustdevs@gmail.com>
2026-02-18 04:15:33 +0700

    add compression of xml draft 1

---
+++ textbooks/cs14net10/ch9/170226/Ch9Soln/XmlCreatorApp/Program.Compressed.cs
@@ -0,0 +1,12 @@
+﻿using TPNS.SharedLibrary;
+using System.IO.Compression;
+partial class Program
+{
+    private static void Compress(string algorithm = "gzip")
+    {
+        //GZipStream gzip_smol_xml_obj = new(xml_file_stream,CompressionLevel.SmallestSize);
+        //GZipStream
+    }
+    //BrotliStream
+}
+
