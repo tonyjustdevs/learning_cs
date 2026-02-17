cdcb6fb959e883cb10e8c8662098f47b0834ca42
tonyjustdevs <tonyjustdevs@gmail.com>
2026-02-17 22:22:14 +0700

    add multi folder creator to ViperFolderCreatorApp

--- textbooks/cs14net10/ch9/170226/Ch9Soln/ViperFolderCreatorApp/Program.cs
+++ textbooks/cs14net10/ch9/170226/Ch9Soln/ViperFolderCreatorApp/Program.cs
@@ -6,19 +6,20 @@ using TPNS.SharedLibrary;
 
 Console.WriteLine("Hello, World!");
 
+string curr_dir = CurrentDirectory;
+// [1] create template to add new folders
+
+// [2A] create path (per call sign)                                 //  [2A-OK] [SINGLE FOLDER] 
+//string curr_call_path = Combine(curr_dir, "test_call_sign");        //  [2A-OK]
+//CreateDirectory(curr_call_path);                                    //  [2A-OK]
+
+// [2b] create mutiple paths (per call sign)                        //  [2B] [MULTI-FOLDERS] 
+string curr_call_path;
 foreach (var call_signs in Viper.CallSigns)
 {
-    //write
-    WriteLine(call_signs);
-
-}
-// [1] create template to add new folders
-string curr_dir = CurrentDirectory;
-
-// [2] create path (per call sign)
-//string curr_call_path = Combine(curr_dir, "test_call_sign"); // ok
-string curr_call_path = Combine(curr_dir, "test_call_sign"); // ok
-CreateDirectory(curr_call_path);
+    curr_call_path = Combine(curr_dir, call_signs);                 //  [2B] OK
+    CreateDirectory(curr_call_path);                                //  [2B]
+} 
 
 // [3] test directory exists
 #if TESTMODE
@@ -32,4 +33,6 @@ if (Directory.Exists(curr_call_path))
     WriteLine($"Folder created succesfully: {curr_call_path}");
 }
 #endif
+
+WriteLine("\nprogram ended...\n");
 // [4]
