50c3c65efb884dc56615bb2d5334d137a8538161
tonyjustdevs <tonyjustdevs@gmail.com>
2026-02-19 00:51:23 +0700

    add v1 xml call sign app 180226

--- textbooks/cs14net10/ch9/170226/Ch9Soln/XmlCreatorApp/Program.Compressed.cs
+++ textbooks/cs14net10/ch9/170226/Ch9Soln/XmlCreatorApp/Program.Compressed.cs
@@ -4,8 +4,7 @@ partial class Program
 {
     private static void Compress(string algorithm = "gzip")
     {
-        //GZipStream gzip_smol_xml_obj = new(xml_file_stream,CompressionLevel.SmallestSize);
-        //GZipStream
+        GZipStream;
     }
     //BrotliStream
 }
---
+++ textbooks/cs14net10/ch9/180226/Ch9Soln/CallSignsXmlApp/Program.cs
@@ -0,0 +1,49 @@
+﻿        using System.Xml;
+        using TPNS.SharedLibrary;
+
+        Console.WriteLine("Welcome To ViperCallSigns App!");
+
+        // create directory
+        string curr_dir = CurrentDirectory;
+
+        // create xml file path
+        string xml_path = Combine(curr_dir, "viper.xml");
+
+        // create filestream
+        using (FileStream xml_fstream = File.Create(xml_path)) { 
+            using (XmlWriter xml_writer = XmlWriter.Create(xml_fstream, new XmlWriterSettings { Indent = true })){ 
+                xml_writer.WriteStartDocument();
+                xml_writer.WriteStartElement("call_signs");
+            
+                foreach (string call_sign in Viper.CallSigns)
+                {
+                    xml_writer.WriteElementString("call_sign", call_sign);
+                }
+                xml_writer.WriteEndElement();
+                xml_writer.WriteEndDocument();
+            }
+        }
+// UnauthorizedAccessException
+// IOException ex
+// Exception ex
+static void output_file_info(string file_path)
+{
+    WriteLine($"Name: {GetFileName(file_path)}");
+    WriteLine($"Directory: {GetDirectoryName(file_path)}");
+    WriteLine($"Size (bytes): {new FileInfo(file_path).Length:N0}");
+    WriteLine("/------------------");
+    WriteLine(File.ReadAllText(file_path));
+    WriteLine("------------------");
+    WriteLine(BitConverter.ToString(File.ReadAllBytes(file_path)));
+    WriteLine("------------------");    
+    foreach (var line in File.ReadAllLines(file_path))
+    {
+        WriteLine(line);
+    };
+    //File.open
+    WriteLine("------------------/");
+}
+
+        output_file_info(xml_path);
+
+        WriteLine("...Program Ended...");
