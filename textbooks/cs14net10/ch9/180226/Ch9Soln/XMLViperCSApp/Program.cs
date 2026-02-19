bdec33bb97b764b8a8dbf3afc8ee9ebdfcc6b405
tonyjustdevs <tonyjustdevs@gmail.com>
2026-02-19 19:32:52 +0700

    update xmlvipercsvapp

--- textbooks/cs14net10/ch9/180226/Ch9Soln/XMLViperCSApp/Program.cs
+++ textbooks/cs14net10/ch9/180226/Ch9Soln/XMLViperCSApp/Program.cs
@@ -1,49 +1,59 @@
-﻿using System.Xml;
+﻿using System.IO.Compression;
+using System.Xml;
 using TPNS.SharedLibrary;
 
 Console.WriteLine("\n ------ Welcome to XML CS App! ------ \n");
 
 // [1] create xml path (DATA LAYER)
-string xml_path = Combine(GetCurrentDirectory(), "viper.xml");
+string input_file_path = Combine(GetCurrentDirectory(), "viper.xml");
+string input_file_path2 = Combine(GetCurrentDirectory(), "viper.txt");
 
+
+// choose (algo == gzip)    -------> gzip___stream -> xml_writer(stream) -> .xml
+// choose else              -------> brotli_stream -> xml_writer(stream) -> .xml
 // [2] create stream (TRANSPORT LAYER)
+// [3] add Compression: GZ
+
 #region [2] FileStream
-using (FileStream xml_fstream = File.Create(xml_path))
+
+
+//FileStream xml_fstream = File.Create(xml_path)
+using (FileStream xml_fstream = File.Create(input_file_path))
+using (XmlWriter xml_writer = XmlWriter.Create(xml_fstream,
+        new XmlWriterSettings { Indent = true }))
 {
-    using (XmlWriter xml_writer = XmlWriter.Create(xml_fstream,
-        new XmlWriterSettings { Indent=true}))
+    try
     {
-		try
-		{
-            // start element??
-            xml_writer.WriteStartDocument();
-            xml_writer.WriteStartElement("call_signs");
+        // start element??
+        xml_writer.WriteStartDocument();
+        xml_writer.WriteStartElement("call_signs");
 
-            foreach (var call_sign in Viper.CallSigns)
-            {
-                xml_writer.WriteElementString("call_sign", call_sign);
-                //WriteLine(call_sign);
-            }
-            xml_writer.WriteEndElement();
-            xml_writer.WriteEndDocument();
-            // write each call sign into element
-        }
-        catch (IOException ex)
-		{
-            WriteLine($"IO ERROR CAUGHT: {ex}");
-		}
-
-        catch (Exception ex)
+        foreach (var call_sign in Viper.CallSigns)
         {
-            WriteLine($"ERROR CAUGHT: {ex}");
+            xml_writer.WriteElementString("call_sign", call_sign);
+            //WriteLine(call_sign);
         }
-
-
+        xml_writer.WriteEndElement();
+        xml_writer.WriteEndDocument();
+        // write each call sign into element
+        //xml_writer.Close();
     }
+    catch (IOException ex)
+    {
+        WriteLine($"IO ERROR CAUGHT: {ex}");
+    }
+
+    catch (Exception ex)
+    {
+        WriteLine($"ERROR CAUGHT: {ex}");
+    }
+
+
 }
+
 #endregion
 
-GetFileInfo(xml_path);
+GetFileInfo(input_file_path);
 
 Console.WriteLine("\n ------ Program Ended! ------ \n");
 
