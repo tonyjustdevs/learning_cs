3bd5a56fe8a3842b13cd19cdf464f26658cbefe7
tonyjustdevs <tonyjustdevs@gmail.com>
2026-02-23 12:19:25 +0700

    add xml serialization app

---
+++ textbooks/cs14net10/ch9/230226/Ch09Soln/SerializationApp/Program.Helpers.cs
@@ -0,0 +1,52 @@
+﻿using System.Xml.Serialization; // To use XmlSerializer.
+using Packt.Shared; // To use Person.
+
+
+partial class Program
+{
+    private static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"*** {title} ***");
+        ForegroundColor = previousColor;
+    }
+    private static void OutputFileInfo(string path)
+    {
+        WriteLine("**** File Info ****");
+        WriteLine($"File: {GetFileName(path)}");
+        WriteLine($"Path: {GetDirectoryName(path)}");
+        WriteLine($"Size: {new FileInfo(path).Length:N0} bytes.");
+        WriteLine("/------------------");
+        WriteLine(File.ReadAllText(path));
+        WriteLine("------------------/");
+    }
+
+    private static void DoSerialization(List<Person> ppl, string file_name)
+    {
+        XmlSerializer ppl_serializer = new(ppl.GetType());
+
+        // create xml
+        string full_file_path = Combine(GetCurrentDirectory(), file_name);
+        WriteLine($"{full_file_path}");
+        using (FileStream fs = new(full_file_path, FileMode.Create))
+        {
+            ppl_serializer.Serialize(fs, ppl);
+        }
+
+        //using (FileStream fs = new()) 
+        //{
+        //    ppl_serializer.Serialize(fs, ppl);
+        //}
+    }
+
+    static void SerializeOnceAgain(List<Person> peeps)
+    {
+        XmlSerializer im_very_xml_serial = new(peeps.GetType());
+
+        using (FileStream fs = File.Create(Combine(GetCurrentDirectory(), "serials_business.xml"))) 
+        {
+            im_very_xml_serial.Serialize(fs, peeps);
+        }
+    }
+}
---
+++ textbooks/cs14net10/ch9/230226/Ch09Soln/SerializationApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml.Serialization; // To use XmlSerializer.
+using Packt.Shared; // To use Person.
+
+List<Person> people = new()
+{
+  new(initialSalary: 30_000M)
+  {
+    FirstName = "Alice",
+    LastName = "Smith",
+    DateOfBirth = new(year: 1974, month: 3, day: 14)
+  },
+  new(initialSalary: 40_000M)
+  {
+    FirstName = "Bob",
+    LastName = "Jones",
+    DateOfBirth = new(year: 1969, month: 11, day: 23)
+  },
+  new(initialSalary: 20_000M)
+  {
+    FirstName = "Charlie",
+    LastName = "Cox",
+    DateOfBirth = new(year: 1984, month: 5, day: 4),
+    Children = new()
+    {
+      new(initialSalary: 0M)
+      {
+        FirstName = "Sally",
+        LastName = "Cox",
+        DateOfBirth = new(year: 2012, month: 7, day: 12)
+      }
+    }
+  }
+};
+//SectionTitle("Serializing as XML");
+//// Create serializer to format a "List of Person" as XML.
+//XmlSerializer xs = new(type: people.GetType());
+//// Create a file to write to.
+//string path = Combine(CurrentDirectory, "people.xml");
+//using (FileStream stream = File.Create(path))
+//{
+//    // Serialize the object graph to the stream.
+//    xs.Serialize(stream, people);
+//} // Closes the stream.
+//DoSerialization(people, "cool_beans.xml");
+SerializeOnceAgain(people);
+//OutputFileInfo(path);
+
+
+//WriteLine($"people.GetType(): {people.GetType()}");
+//WriteLine($"new Person(): {new Person().GetType()}");
+
