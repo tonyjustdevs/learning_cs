715341700043f44ed46e239826ac6f29964b2393
tonyjustdevs <tonyjustdevs@gmail.com>
2026-03-03 12:16:19 +0700

    add a bad shape class

---
+++ textbooks/cs14net10/ch9/030326_Serialization/Ch9ExercisesSoln/SerializingShapesCApp/BadShape.cs
@@ -0,0 +1,21 @@
+﻿
+namespace SerializingShapesCApp;
+
+public abstract class BadShape
+{
+    public double Width{ get; set; }
+    public double Height{ get; set; }
+    public string? Color{ get; set; }
+
+    public abstract double Area { get; }
+}
+
+public class Rectangle : BadShape
+{
+    public override double Area => Width * Height;
+}
+
+public class Circle : BadShape
+{
+    public override double Area => (Width) * double.Pi;
+}
---
+++ textbooks/cs14net10/ch9/030326_Serialization/Ch9ExercisesSoln/SerializingShapesCApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using SerializingShapesCApp;
+Console.WriteLine("Hello Serializer");
+
+// Create a list of Shapes to serialize.
+List<Shape> listOfShapes = new()
+{
+    new Circle { Color = "Red", Radius = 2.5 },
+    new Rectangle { Color = "Blue", Height = 20.0, Width = 10.0 },
+    new Circle { Color = "Green", Radius = 8.0 },
+    new Circle { Color = "Purple", Radius = 12.3 },
+    new Rectangle { Color = "Blue", Height = 45.0, Width = 18.0 }
+};
+
+//List<Shape> loadedShapesXml =
+//    serializerXml.Deserialize(fileXml) as List<Shape>;
+
+//foreach (Shape item in loadedShapesXml)
+//{
+//    WriteLine("{0} is {1} and has an area of {2:N2}",
+//        item.GetType().Name, item.Color, item.Area);
+//}
+
+
+//Loading shapes from XML:
+//Circle is Red and has an area of 19.63
+//Rectangle is Blue and has an area of 200.00
+//Circle is Green and has an area of 201.06
+//Circle is Purple and has an area of 475.29
+//Rectangle is Blue and has an area of 810.00
