744a08964c65edfeccb3cad9419cdcf1d2e72584
tonyjustdevs <tonyjustdevs@gmail.com>
2026-03-30 22:00:50 +0700

    add mcp server and test agent model with github copilot sucessfully

---
+++ webdev/matureweb/random_testing/300326/Chapter01/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// https://github.com/markjprice/markjprice/blob/main/articles/getting-help.md
