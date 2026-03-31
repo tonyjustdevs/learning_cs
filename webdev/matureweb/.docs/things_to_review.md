cb2d949a4483bdd950fc3da50dd3cac76851de21
tonyjustdevs <tonyjustdevs@gmail.com>
2026-03-31 22:25:42 +0700

    add regions to program.cs

--- webdev/matureweb/.docs/things_to_review.md
+++ webdev/matureweb/.docs/things_to_review.md
@@ -3,7 +3,7 @@
     - Under the hood: LINQ works via deferred execution and iterators. Methods like Where and Select don’t immediately produce a result; they return an IEnumerable<T> that, when iterated, will yield the filtered or transformed results on the fly.
     - This means LINQ queries are memory-efficient (they don’t necessarily create new lists until you force an evaluation, e.g., by calling .ToList() or iterating in a foreach). It also means you can compose queries dynamically.
 - Delegates enable us to be flexible in design, allowing us to swap behavior at runtime without changing our class structure.
-
+- `MapStaticAssets`: has an issue with working with static HTML files when you have Visual Studio features like Browser Link and Hot Reload.
 
 Misc:
 - Combined with yield, the IEnumerable/IEnumerator interfaces implement the **Iterator pattern**.
--- webdev/matureweb/ch1/290326/MatureWeb/MatureWebSoln/TP.Mvc/Program.cs
+++ webdev/matureweb/ch1/290326/MatureWeb/MatureWebSoln/TP.Mvc/Program.cs
@@ -2,47 +2,88 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TP.Mvc.Data;
-#endregion 
+#endregion
 
+#region [1a] Summary
+// summary:
+// - the incoming request is
+// - handled by the configured HTTP request pipeline,
+// - then by a route handler,
+// - and then by a controller,
+// - which creates a model and passes it to a view.
+//
+// The letters MVC are not in the order of processing,
+// and there is more to MVC than just models, views, and controllers.
+#endregion
+
+#region [2] Create builder
 var builder = WebApplication.CreateBuilder(args);
+WriteLine($"\n[1] Builder created: {builder} [{builder.GetType()}]");
+#endregion
 
-// Add services to the container.
+#region [3a] Add builder.Configuration (more like get it but yeh)
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+WriteLine($"\n[2a]Retrieving connection string via builder.Configuration: [{builder.Configuration}]");
+WriteLine($"[2b]- ConnStr: [{connectionString}]");
+#endregion
 
+#region [3b] Add builder.Services
+WriteLine("\n[3] Adding Auth-DbContext with connStr via Services.AddDbContext...UseSqlServer(): {}");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+#endregion
 
+#region [4] Build app
 var app = builder.Build();
+#endregion
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
+#region [5] Configure HTTP pipelines, static-files, redirection, etc
+// AKA Middleware: 
+// - HTTP request -> middleware (here) -> infrastructure action (DB migration)
+#endregion
+
+#region [5a] Configure Environment-Specific Settings: Developer vs Production
+if (app.Environment.IsDevelopment())  // aka checks IsDevelopment==true
+{   
+    app.UseMigrationsEndPoint(); // equivalent to context.Database.Migrate();
+    // - Exposes internal system ops via HTTP
+    // - Uses middleware as a [control-layer]
+    // - Connects [web requests] -> [database operations]
+    // - autoruns: app.UseDeveloperExceptionPage();
 }
-else
-{
+else // [5aii] Production Settings 
+{// The default HSTS value is 30 days. Change for Prod cases: https://aka.ms/aspnetcore-hsts.
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+#endregion
+
+#region [5b] Configure Environment Settings: General/Common
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
+//app.UseStaticFiles();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    name: "default", // setting generic controller pattern
+    pattern: "{controller=Home}/{action=Index}/{id?}") 
     .WithStaticAssets();
 
-app.MapRazorPages()
+app.MapRazorPages() // for auth page
    .WithStaticAssets();
+#endregion
 
+#region [6] Add Routes
+// learn https://learn.microsoft.com/en-us/aspnet/core/fundamentals/environments
+app.MapGet("/env", () => { WriteLine($"\napp.Environment: {app.Environment};"); });
+#endregion
+
+#region [6] Run app
 app.Run();
+#endregion
