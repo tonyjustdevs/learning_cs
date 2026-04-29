bb870e221cf6e0e7acb756bb34b795b3a18eb575
tonyjustdevs <tonyjustdevs@gmail.com>
2026-04-29 08:33:23 +0700

    add asynchttp3

---
+++ multithreading/130426/LearningMultithreading/AsyncHttp3App/Program.Helpers.cs
@@ -0,0 +1,51 @@
+﻿partial class Program
+{
+
+    static void ShowThreadInfo()
+    {
+        Console.WriteLine($"[" +
+            $"nm: {Thread.CurrentThread.Name}, " +
+            $"pr: {Thread.CurrentThread.Priority}, " +
+            $"st: {Thread.CurrentThread.ThreadState}, " +
+            $"tp: {Thread.CurrentThread.IsThreadPoolThread}, " +
+            $"bg: {Thread.CurrentThread.IsBackground}], " +
+            $"id: {Thread.CurrentThread.ManagedThreadId}]");
+    }
+
+    static async Task DoMultiRequests(List<Task<string>> tasks)
+    {
+
+        await foreach (var completedTask in Task.WhenEach(tasks))
+        {
+            try
+            {
+                string res = await completedTask; // what does this line do?
+                ShowThreadInfo();
+                Console.WriteLine(res.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    static List<Task<string>> GetURLTasks()
+    {
+        HttpClient http_client = new();
+
+        List<string> urls = [
+            "https://api.github.com",
+    "https://jsonplaceholder.typicode.com/posts/1",
+    "https://jsonplaceholder.typicode.com/posts/2"
+        ];
+
+        List<Task<string>> tasks = new List<Task<string>>();
+
+        foreach (var url in urls)
+        {
+            tasks.Add(http_client.GetStringAsync(url));
+        }
+        return tasks;
+    }
+}
---
+++ multithreading/130426/LearningMultithreading/AsyncHttp3App/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Starting Async3HTTPApp");
+
+
+//var tasks = GetURLTasks();
+//await DoMultiRequests(tasks);
+
+//var task = Task.Factory.StartNew(CallWebService).ContinueWith((PreviousTask) => CallStoredProc(PreviousTask.Result));
+var task= Task.Factory.StartNew(CallWebService).
+//var task= Task.Run(CallWebService).
+    ContinueWith((PreviousTask) => CallStoredProc(PreviousTask.Result));
+Console.WriteLine("some cool cpu tasks 1");
+Console.WriteLine("some cool cpu tasks 2");
+Thread.Sleep(1000);
+Console.WriteLine("some cool cpu tasks 3");
+Console.WriteLine("some cool cpu tasks 4");
+var res = await task;
+Console.WriteLine($"res: {res}");
+//Console.WriteLine("task started");
+//Console.WriteLine($"task.Status: {task.Status}");
+//Console.WriteLine($"task.IsCompleted: {task.IsCompleted}");
+//Console.WriteLine($"task.IsFaulted: {task.IsFaulted}");
+//Console.WriteLine($"task.IsCanceled: {task.IsCanceled}");
+//Console.WriteLine($"task.Exception: {task.Exception}");
+
+//Thread.Sleep(8000);
+//Console.WriteLine("task completed");
+//Console.WriteLine($"task.Status: {task.Status}");
+//Console.WriteLine($"task.IsCompleted: {task.IsCompleted}");
+//Console.WriteLine($"task.IsFaulted: {task.IsFaulted}");
+//Console.WriteLine($"task.IsCanceled: {task.IsCanceled}");
+//Console.WriteLine($"task.Exception: {task.Exception}");
+
+
+//await task;
+decimal CallWebService() 
+{
+    Console.WriteLine("[WebServer() Started]");
+    ShowThreadInfo();
+    Thread.Sleep(Random.Shared.Next(2000, 4000));
+    Console.WriteLine("[WebServer() Ended]");
+    return 420.69M;
+};
+
+string CallStoredProc(decimal input_money) 
+{
+    Console.WriteLine("[CallStoredProc() Started]");
+    ShowThreadInfo();
+    Thread.Sleep(Random.Shared.Next(2000, 4000));
+    //Console.WriteLine($"You can buy 666 pokemon cards with ${input_money} (from webservice).");
+    Console.WriteLine("[CallStoredProc() Ended]");
+    return $"You can buy 666 pokemon cards with ${input_money} (from webservice).";
+}
